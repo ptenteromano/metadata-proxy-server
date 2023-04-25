fe2e54a3199b74c3de75439c4fab0f9084f03a0a
Philip Tenteromano <tenteromano.p@gmail.com>
2023-04-24 20:51:11 -0400

    clean up code and remove deprecated endpoint

--- meta_server.rb
+++ meta_server.rb
@@ -6,41 +6,24 @@ get "/" do
   "Nothing here!"
 end
 
-# Deprecated
-get "/metadata" do
-  url = params[:url]
-  return halt 400, { error: "No URL provided" }.to_json if url.nil?
-
-  url = "http://" + url if url[0..3] != "http"
-
-  m = MetaInspector.new(url)
-  properties = m.meta_tags["property"]
-
-  {
-    title: properties["og:title"],
-    description: properties["og:description"],
-    image: m.images.best,
-    url: m.url,
-    siteName: properties["og:site_name"],
-    hostname: m.host
-  }.to_json
-rescue => e
-  { error: "invalid url, #{e.message}" }.to_json
-end
-
 # All in one version
 post "/metadata" do
   request_data = JSON.parse(request.body.read, symbolize_names: true)
 
-  metadata_results = []
+  { success: true, data: collect_metadata(request_data[:data]) }.to_json
+rescue => e
+  { error: "invalid url, #{e.message}" }.to_json
+end
 
-  request_data[:data].each do |url|
+# Read through all urls and collect metadata
+def collect_metadata(data)
+  data.map do |url|
     url = "http://" + url if url[0..3] != "http"
 
     m = MetaInspector.new(url)
     properties = m.meta_tags["property"]
 
-    metadata_results << {
+    {
       title: properties["og:title"],
       description: properties["og:description"],
       image: m.images.best,
@@ -49,8 +32,4 @@ post "/metadata" do
       hostname: m.host
     }
   end
-
-  { success: true, data: metadata_results }.to_json
-rescue => e
-  { error: "invalid url, #{e.message}" }.to_json
 end
